a4c043489fad39022c3903aaa3c4540d0c5db0e9
Alexander Chang <gxchang@gmail.com>
2016-07-18 15:52:31 -0700

    set heroku secret .env

--- node-server/app.js
+++ node-server/app.js
@@ -97,6 +97,6 @@ app.get('*', function(req, res) {
   res.redirect('/');
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port 3000...")
 });
